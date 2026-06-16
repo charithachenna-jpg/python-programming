703b148ca2cf2d233425d2f538d8a3d1c344aa43
charithachenna-jpg <charithachenna@gmail.com>
2026-06-16 11:36:13 +0500

    Delete 8 directory

--- 8/8.7.py
+++
@@ -1,3 +0,0 @@
-import secrets
-random_number=secrets.randbelow(10)+1
-print("Cryptographically secure random number:",random_number)
--- 8/8.8.py
+++
@@ -1,7 +0,0 @@
-import platform
-system_name=platform.system()
-node_name=platform.node()
-processor_name=platform.processor()
-print("sytem name:",system_name)
-print("node name:",node_name)
-print("processor name:",processor_name)

f953c6d965689643125d0948d71f3cea2951df90
charithachenna-jpg <charithachenna@gmail.com>
2026-06-16 11:37:24 +0500

    Delete 10 directory

--- 10/10.3.py
+++
@@ -1,9 +0,0 @@
-try:
-         num=int(input("Enter a number:"))
-         result=10/num
-except ValueError:
-         print("Error:Invalid input! Please enter a valid number")
-except ZeroDivisionError:
-         print("Error:Division by Zero")
-else:
-         print("Result:",result)
--- 10/10.4.py
+++
@@ -1,6 +0,0 @@
-try:
-         file=open("output.txt","r")
-except IOError:
-         print("Error:Unable to read the file")
-finally:
-         file.close()
